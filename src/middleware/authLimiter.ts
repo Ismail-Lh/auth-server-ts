cf699ac22e325e190d0210215f95d3c3b6d98557
Ismail-Lh <ismailsaad03@gmail.com>
2024-08-30 18:18:39 +0100

    refactor: Update rate limiter middleware for authentication requests by adding comment

--- src/middleware/authLimiter.ts
+++ src/middleware/authLimiter.ts
@@ -1,5 +1,18 @@
 import { rateLimit } from 'express-rate-limit';
 
+/**
+ * Rate limiter middleware for authentication requests.
+ *
+ * @remarks
+ * This middleware limits the number of requests allowed within a specific time window.
+ *
+ * @param windowMs - The time window in milliseconds.
+ * @param max - The maximum number of requests allowed within the time window.
+ * @param skipSuccessfulRequests - Determines whether successful requests should be skipped from rate limiting.
+ *
+ * @returns The rate limiter middleware.
+ */
+
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 20,
